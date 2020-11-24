95dcaf65399a9118edd807461d10e1d9264c6b2b
JackCX <jackcx777@gmail.com>
2020-11-24 21:01:16 +0300

    Database seted to PostgreSQL in settings.py. All migrations completed by command python manage.py migrate. Server successfully started by command python manage.py runserver.

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'django_ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommercedb',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
