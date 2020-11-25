fec50c2245075aa6ce7c387d7b1570b8bbf12dec
JackCX <jackcx777@gmail.com>
2020-11-25 22:32:03 +0300

    Static files and folder added in django_ecommerce folder. File settings.py edited. Note: in file settings.py added os module import and skipped command python manage.py collectstatic - than ststic image on home page works! File home.html edited. Note: width="100%" added in img tag.

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,4 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'django_ecommerce/static/')
+]
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
