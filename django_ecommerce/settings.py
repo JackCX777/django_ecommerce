16d335b43b9ed6638cb8c068e6cd9e4882a33d1f
JackCX <jackcx777@gmail.com>
2020-12-08 17:55:17 +0300

    Signup page now looks good.

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -137,3 +138,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- shop/urls.py
+++ shop/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('cart/add/<int:product_id>', views.add_cart, name='add_cart'),
     path('cart/remove/<int:product_id>', views.cart_remove, name='cart_remove'),
     path('cart/remove_product/<int:product_id>', views.cart_remove_product, name='cart_remove_product'),
-    path('account/create/', views.signup_view, name='signup'),
+    path('account/create', views.signup_view, name='signup'),
 ]
